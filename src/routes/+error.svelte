1c5d465b24ec2d05fb955702dac0c1c046c3049b
JunederZ <arjunadimas000@gmail.com>
2025-01-20 11:03:26 +0700

    feat: Error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+
+    import { page } from "$app/state"
+
+</script>
+
+<div class="flex flex-col items-center justify-center h-screen bg-slate-950 text-white">
+
+    <h1 class="text-4xl font-bold text-center">
+        Unexpected error!
+    </h1>
+    <h2 class="text-2xl font-bold text-center">
+        I'm sorry :(
+    </h2>
+
+</div>
